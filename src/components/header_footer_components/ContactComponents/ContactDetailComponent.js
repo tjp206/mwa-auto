18018bb8ceb96a64ffb32529833d63f66ac406f8
TJ <tjpataria@TJs-MacBook-Pro.local>
2021-01-18 15:52:26 +0000

    added contact & service components

---
+++ src/components/header_footer_components/ContactComponents/ContactDetailComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ContactDetail.css'
+
+const ContactDetailComponent = () => {
+    return(
+
+    )
+}
+
+export default ContactDetailComponent;
---
+++ src/components/header_footer_components/ServiceComponents/ServiceDetailComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ServiceDetail.css'
+
+const ServiceDetailComponent = () => {
+    return(
+
+    )
+}
+
+export default ServiceDetailComponent;
