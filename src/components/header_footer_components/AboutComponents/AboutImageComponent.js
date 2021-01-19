3218ce5d70d50bf93564ccfc5b3900b61ab80fc7
TJ <tjpataria@TJs-MacBook-Pro.local>
2021-01-19 12:37:30 +0000

    added carousel to image component

--- src/components/header_footer_components/AboutComponents/AboutImageComponent.js
+++ src/components/header_footer_components/AboutComponents/AboutImageComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Slider from 'infinite-react-carousel';
 import './AboutImage.css'
 
 const AboutImageComponent = () => {
     return (
-        <>
+        <Slider dots>
             <img className="car" src="/a3.jpg" alt="Audi A3"/>
             <img className="car" src="/passat.jpg" alt="Volkswagen Passat"/>
             <img className="car" src="/range.jpg" alt="Range Rover Sport"/>
             <img className="car" src="/van.jpg" alt="Van/Truck"/>
-        </>
+        </Slider>
     )
 }
 
